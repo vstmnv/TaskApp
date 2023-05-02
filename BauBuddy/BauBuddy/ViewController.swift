cc20deffd822fadd9d69a8fe173e5b7e3c821e3c
Vesela Stamenova <vstamenova@appolica.com>
2023-05-02 12:34:55 +0300

    Add: Created the project

---
+++ BauBuddy/BauBuddy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BauBuddy
+//
+//  Created by Vesela Stamenova on 2.05.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
